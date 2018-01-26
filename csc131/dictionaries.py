ccecf5aca92c07af979020eda42ef9bffeae7e7f
charlesjhall <hall519@live.missouristate.edu>
2018-01-26 13:13:36 -0600

    Initial import of CSC 131 package

---
+++ csc131/dictionaries.py
@@ -0,0 +1,7 @@
+def get_personal_data()->dict:
+    """
+
+    :return: Returns a dictionary of personal information
+    """
+    personal_data = {"name":"Charles","role":"student"}
+    return personal_data
