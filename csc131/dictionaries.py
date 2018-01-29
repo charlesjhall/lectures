9d6b3a4b1e19e6dbf2c8adb98cedc72f62deead1
charlesjhall <hall519@live.missouristate.edu>
2018-01-29 13:13:20 -0600

    Initial import of CSC 131 package

--- csc131/dictionaries.py
+++ csc131/dictionaries.py
@@ -1,7 +1,32 @@
+import string
+
 def get_personal_data()->dict:
     """
 
     :return: Returns a dictionary of personal information
     """
-    personal_data = {"name":"Charles","role":"student"}
+    personal_data = {"name":"Charles","a_role":"student"}
     return personal_data
+
+def main() -> int:
+    default_dict = dict()
+    print(default_dict)
+    initialized_dict = dict([('name', 'Charles'), ('a_role', 'Joker')])
+    simple_init_dict = dict(name='Charles', a_role='student')
+    print(initialized_dict)
+    print(simple_init_dict)
+    simple_init_dict['a_role'] = 'Joker'
+    print(simple_init_dict)
+    my_comprehension = {x:x**2 for x in range(5)}
+    print(my_comprehension)
+    s = "Mary had a little,".translate({ord(i): None for i in string.punctuation})
+    print(s)
+
+
+    return 0
+
+
+
+
+if __name__ == '__main__':
+    main()
